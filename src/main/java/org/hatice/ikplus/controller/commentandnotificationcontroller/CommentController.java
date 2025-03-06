c9d5b9148037d1f14544f9c2150090920a3d5774
berkaybasol <berkaybasoll@gmail.com>
2025-03-06 12:20:33 +0300

    hfdghfd

--- src/main/java/org/hatice/ikplus/controller/commentandnotificationcontroller/CommentController.java
+++ src/main/java/org/hatice/ikplus/controller/commentandnotificationcontroller/CommentController.java
@@ -8,10 +8,7 @@ import org.hatice.ikplus.dto.response.BaseResponse;
 import org.hatice.ikplus.entity.commentandnotificationmanagement.Comment;
 import org.hatice.ikplus.service.commentandnotificationservice.CommentService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
